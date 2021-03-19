4b5bac8a0fb349d742cb87591e01b433d45c25d3
Marques <MBatoon1996@gmail.com>
2021-03-19 06:18:24 -1000

    added Bootstrap and react-router-dom. Setup Switch Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return(
+    <Router>
+      <Switch>
+        <Route path="/" exact render={() => <h1>Pokedex</h1>} />
+        <Route render={() => <h1>404 Not Found</h1>} />
+      </Switch>
+    </Router>
   );
 }
 
