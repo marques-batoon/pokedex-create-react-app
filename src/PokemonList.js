ef0f46827dd907551bd6b28548f26d7da38e2065
Marques <MBatoon1996@gmail.com>
2021-03-24 02:58:56 -1000

    grabs api from region link and prints list of all mons in that region but does not print in correct numerical order

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { checkStatus, json } from './utils/fetchUtils';
+import { Link } from 'react-router-dom';
 
 class PokemonList extends React.Component {
     constructor(props) {
@@ -7,16 +8,27 @@ class PokemonList extends React.Component {
 
         const params = new URLSearchParams(props.location.search);
 
-        console.log(params.get('region'));
+        //console.log(params.get('region'));
 
         this.state={
             mons: [],
             region: params.get('region') || 'kanto',
+            monName: '',
         }
     }
 
     componentDidMount() {
         this.getPokemonList();
+    }
+
+    clickedMon = (event) => {
+        event.preventDefault();
+        this.setState({ monName: event.target.value });
+
+        const waitSecs = 250 // 0.25 seconds
+        setTimeout(() => {
+          window.location.href=`/pokemon?mon=${event.target.value}`;
+        },waitSecs);
     }
 
     getPokemonList = () => {
@@ -29,29 +41,36 @@ class PokemonList extends React.Component {
                 throw new Error(data.error);
             }
             console.log(data.main_generation.url);
-            fetch(data.main_generation.url)
-            .then(checkStatus)
-            .then(json)
-            .then(data1 => {
-                if(data1.error) {
-                    throw new Error(data.error);
-                }
-                console.log(data1.pokemon_species);
-                console.log(data1.pokemon_species[0]);
-                this.setState({ mons: data1.pokemon_species });
-            })
-            .catch(error => console.log(error.message));
+            this.setState({ listLink: data.main_generation.url });
+            this.getList();
         })
         .catch(error => console.log(error.message));
     }
 
+    getList = () => {
+        console.log(this.state.listLink);
+        fetch(this.state.listLink)
+        .then(checkStatus)
+        .then(json)
+        .then(data => {
+            if(data.error) {
+                throw new Error(data.error);
+            }
+            console.log(data.pokemon_species);
+            this.setState({ mons: data.pokemon_species });
+            
+        })
+    }
+
     render() {
-        const{ region } = this.state;
+        const{ region, monName, mons } = this.state;
 
         return(
             <React.Fragment>
                 <h1>{region}</h1>
-                
+                <div className="list-group">
+                {mons.map(mon => <Link to={`/pokemon?name=${monName}`}key={mon.name}><button type="button" className="list-group-item list-group-item-action text-center" value={mon.name} onClick={this.clickedMon}>{mon.name}</button></Link>)}
+                </div>
             </React.Fragment>
         )
     }
